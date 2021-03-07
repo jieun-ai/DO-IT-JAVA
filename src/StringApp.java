39e348afff8c04fde9f5e21aefa00af41ce64a20
qweas <qweas@DESKTOP-9S5ESI3>
2021-03-07 12:53:00 +0900

    JAVA study

---
+++ src/StringApp.java
@@ -0,0 +1,21 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		//Character(문자) VS String(문자열) 
+		//자바에서는 큰따옴표 사용 작은 따옴표는 사용잘 안함
+		
+		System.out.println("Hello World"); //String
+		System.out.println('H'); //Character
+		System.out.println("H");
+		
+		//줄바꿈  \n
+		System.out.println("Hello \nWorld");
+		
+		//escape
+		System.out.println("Hello \"World\""); //Hello "World"
+			
+	}
+
+}
---
+++ src/StringOperation.java
@@ -0,0 +1,10 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World".length());// 몇글자?
+		System.out.println("Hello,leezche ...bye");
+
+	}
+
+}
