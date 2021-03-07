d52dfdfa4cc5df49929abedc4355412263b26382
jieun-ai <75363345+jieun-ai@users.noreply.github.com>
2021-03-07 14:04:15 +0900

    Add files via upload

---
+++ src/StringOperation.java
@@ -0,0 +1,12 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World".length());// 몇글자?
+		System.out.println("Hello,leezche ...bye".replace("leezche", "egoing"));
+	
+		
+		
+	}
+
+}
