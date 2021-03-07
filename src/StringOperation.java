35990d058f02fd63db4d670083a78009886744ca
qweas <qweas@DESKTOP-9S5ESI3>
2021-03-07 13:18:13 +0900

    .

--- src/StringOperation.java
+++ src/StringOperation.java
@@ -4,7 +4,7 @@ public class StringOperation {
 	public static void main(String[] args) {
 		System.out.println("Hello World".length());// 몇글자?
 		System.out.println("Hello,leezche ...bye".replace("leezche", "egoing"));
-
+		System.out.println("Hello \"World\""); //Hello "World"
 	}
 
 }
