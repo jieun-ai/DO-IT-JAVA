f5fe2e33bb0f973390c8b1cf9e70efaaddc923eb
qweas <qweas@DESKTOP-9S5ESI3>
2021-03-07 13:11:23 +0900

    JAVA study

--- src/StringOperation.java
+++ src/StringOperation.java
@@ -3,7 +3,7 @@ public class StringOperation {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World".length());// 몇글자?
-		System.out.println("Hello,leezche ...bye");
+		System.out.println("Hello,leezche ...bye".replace("leezche", "egoing"));
 
 	}
 
